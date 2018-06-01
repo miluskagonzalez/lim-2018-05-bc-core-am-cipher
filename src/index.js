891aba8a32de4e6d0f1cf57395ede9cfba76071f
spacedgatito <miluska.gonzalezar@gmail.com>
2018-06-01 15:34:40 -0500

    Subiendo cambios JS y CSS

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
+// Declarando variables
 const encodeButton = document.getElementById('encode');
 const decodeButton = document.getElementById('decode');
 const textInput = document.getElementById('textInput');
 const shift = document.getElementById('shift');
 const textOutput = document.getElementById('textOutput');
-
+// Llamando a la función cifrar
 encodeButton.addEventListener('click', ()=>{
   textOutput.innerHTML = cipher.encode(parseInt(shift.value), textInput.value);
 })
-
+// Llamando a la función decifrar
 decodeButton.addEventListener('click', ()=>{
   textOutput.innerHTML = cipher.decode(parseInt(shift.value), textInput.value);
 })
